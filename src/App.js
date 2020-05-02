09346068cd9cbba1be37bf2f9312b7c04e48fa08
Your Name <xfitex>
2020-05-02 19:58:56 -0300

    first commit Creacion del Repositorio

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <main>
+    <div className="">
+     hello ow
+    </div>
+    <img src="logo.png"></img>
+    </main>
+  );
+}
+
+export default App;
